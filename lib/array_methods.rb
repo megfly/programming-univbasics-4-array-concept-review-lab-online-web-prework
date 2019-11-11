113122c86ab6c1dc70902379c9868288bb72e63a
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-11 23:31:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
-  array.index(value_to_find [count])
-  count +=1
+    array.index(value_to_find [count])
+    count +=1
 end
 
 def find_max_value(array)
