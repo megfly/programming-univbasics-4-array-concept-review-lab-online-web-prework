d8b725963f0631d480c30e55a02665ae7fe8307a
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-12 00:10:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,5 +22,11 @@ end
 
 
 def find_min_value(array)
-  # Add your solution here
+  x = array[0]
+  array.length.times { |index|
+    if array[index] < x 
+      x = array[index]
+    end 
+  }
+  x
 end
