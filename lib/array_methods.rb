139faa0a57e583eb690efbf23d0f56d9334ccfd8
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-12 00:00:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-array.length.times do |count|
+  array.length.times do |count|
   if array[count] == value_to_find
   return count
-end
- nil
+  end
+  nil
 end
 
 
